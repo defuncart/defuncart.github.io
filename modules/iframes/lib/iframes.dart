4df6764d9a1853cf71cd5cd4b8190a8a4f97613c
James Leahy <defuncart@gmail.com>
2020-11-26 15:29:58 +0100

    Add iframes module.

---
+++ modules/iframes/lib/iframes.dart
@@ -0,0 +1,6 @@
+library iframes;
+
+export 'src/widgets/bandcamp_widget.dart';
+export 'src/widgets/mixcloud_widget.dart';
+export 'src/widgets/vimeo_widget.dart';
+export 'src/widgets/youtube_widget.dart';
---
+++ modules/iframes/lib/src/widgets/mixcloud_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+
+import 'i_frame_widget.dart';
+
+class MixcloudWidget extends StatelessWidget {
+  static const minWidth = 375;
+
+  final String id;
+  final double width;
+  final double height;
+
+  const MixcloudWidget({
+    Key key,
+    @required this.id,
+    this.width,
+    this.height = 120,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final widthAsString = width?.toString() ?? '100%';
+    return IFrameWidget(
+      content:
+          '<iframe width="$widthAsString" height="$height" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&amp;feed=%2Fstrawberrycomplexity%2F$id%2F" frameborder="0"></iframe>',
+      viewId: id,
+      height: height + 20,
+      width: width + 20,
+    );
+  }
+}
