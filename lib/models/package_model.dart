7178051092f84e0a9eb91566974538dc74ad3fde
James Leahy <defuncart@gmail.com>
2020-11-27 16:40:08 +0100

    Add PackageModel.

---
+++ lib/models/package_model.dart
@@ -0,0 +1,15 @@
+import 'package:meta/meta.dart';
+
+class PackageModel {
+  const PackageModel({
+    @required this.title,
+    @required this.description,
+    @required this.pubDevUrl,
+    @required this.gitHubUrl,
+  });
+
+  final String title;
+  final String description;
+  final String pubDevUrl;
+  final String gitHubUrl;
+}
