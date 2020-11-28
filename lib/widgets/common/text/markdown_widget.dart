cb43e44c153685999a5b0c58bb2683b561dcc1a6
James Leahy <defuncart@gmail.com>
2020-11-28 14:01:03 +0100

    Add MarkdownWidget, MarkdownScreen.

---
+++ lib/widgets/common/text/markdown_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/style.dart';
+import 'package:markdown/markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MarkdownWidget extends StatelessWidget {
+  const MarkdownWidget({
+    Key key,
+    this.data,
+  }) : super(key: key);
+
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Html(
+      data: markdownToHtml(data),
+      onLinkTap: (url) => launch(url),
+      style: {
+        'body': Style(
+          margin: const EdgeInsets.all(0),
+        ),
+      },
+    );
+  }
+}
