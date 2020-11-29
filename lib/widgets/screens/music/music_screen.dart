7e9e24262f57cf68ae5b8c9136cf450e5e8b2909
James Leahy <defuncart@gmail.com>
2020-11-29 18:03:28 +0100

    Update MusicScreen.

--- lib/widgets/screens/music/music_screen.dart
+++ lib/widgets/screens/music/music_screen.dart
@@ -1,5 +1,7 @@
+import 'package:defuncart_github_io/configs/styling.dart';
 import 'package:defuncart_github_io/configs/website_content.dart';
-import 'package:defuncart_github_io/widgets/common/images/clickable_image.dart';
+import 'package:defuncart_github_io/models/music_act_model.dart';
+import 'package:defuncart_github_io/widgets/common/gesture_detector/mouse_gesture_detector.dart';
 import 'package:defuncart_github_io/widgets/common/responsive/webpage.dart';
 import 'package:defuncart_github_io/widgets/routing/routing.dart';
 import 'package:flutter/material.dart';
@@ -15,31 +17,72 @@ class MusicScreen extends StatelessWidget {
     return Webpage(
       content: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        // mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(WebsiteContent.music.content),
-          SizedBox(height: 8.0),
+          SizedBox(height: 16.0),
           Wrap(
             spacing: 8.0,
             runSpacing: 8.0,
             children: [
-              for (final act in WebsiteContent.music.acts)
-                Column(
-                  children: [
-                    ClickableImage(
-                      act.artworkPath,
-                      width: 350,
-                      height: 350,
-                      onPressed: () => RoutePageManager.of(context).setNewRoutePath(act.relativeUrl),
-                    ),
-                    SizedBox(height: 8.0),
-                    Text(act.title),
-                  ],
-                ),
+              for (final act in WebsiteContent.music.acts) Act(act: act),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class Act extends StatelessWidget {
+  const Act({
+    Key key,
+    @required this.act,
+  }) : super(key: key);
+
+  final MusicActModel act;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseGestureDetector(
+      child: SizedBox(
+        width: 375,
+        child: Card(
+          elevation: Styling.cardElevation,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  act.title,
+                  style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16.0),
+                Image.asset(
+                  act.artworkPath,
+                  width: 200,
+                  height: 200,
+                ),
+                SizedBox(height: 16.0),
+                Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  children: [
+                    for (final genre in act.genres)
+                      Text(
+                        genre,
+                        style: TextStyle(
+                          letterSpacing: -0.5,
+                          color: Theme.of(context).disabledColor,
+                        ),
+                      )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      onTap: () => RoutePageManager.of(context).setNewRoutePath(act.relativeUrl),
     );
   }
 }
